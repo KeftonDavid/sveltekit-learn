0c29f5d6da38c2d2ee9842931d6c72030cf744a8
KeftonDavid <keftondavid@gmail.com>
2025-08-04 17:59:46 -0300

    build: daisyUi

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,4 +2,35 @@
     let { children } = $props();
     import "../app.css";
 </script>
-{@render children()}
+<div class="navbar bg-base-100 shadow-sm">
+    <div class="navbar-start">
+      <div class="dropdown">
+        <div tabindex="0" role="button" class="btn btn-ghost lg:hidden">
+          <svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"> <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h8m-8 6h16" /> </svg>
+        </div>
+        <!-- svelte-ignore a11y_no_noninteractive_tabindex -->
+        <ul
+          tabindex="0"
+          class="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow">
+          <li><a href="/">Home</a></li>
+          
+
+        </ul>
+      </div>
+      <!-- svelte-ignore a11y_missing_attribute -->
+      <a class="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+    <div class="navbar-center hidden lg:flex">
+      <ul class="menu menu-horizontal px-1">
+        <li><a href="/">Home</a></li>
+        
+      </ul>
+    </div>
+    <div class="navbar-end">
+      <!-- svelte-ignore a11y_missing_attribute -->
+      <a class="btn">Button</a>
+    </div>
+  </div>
+  <div class="container">
+      {@render children()}
+  </div>
